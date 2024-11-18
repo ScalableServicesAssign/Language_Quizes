6dd36a5ad472df4320102a28cefd9bb5ebe1e82f
Pawar <amit.pawar@worldline.com>
2024-11-18 12:16:50 +0530

    docker file added

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const PORT = 3000;
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/quizdb', {
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/quizdb';
+
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log("Connected to MongoDB"))
